98b1689b8ebfc410cbf6166a406dcfce148897c6
ShanzaFatima <shanzafatima70@gmail.com>
2022-12-06 15:24:22 +0500

    share functionality and deletion functionality
    done

--- app/src/main/java/com/ono/locatporium/SavedActivitiesAdapter.java
+++ app/src/main/java/com/ono/locatporium/SavedActivitiesAdapter.java
@@ -11,10 +11,13 @@ import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class SavedActivitiesAdapter extends RecyclerView.Adapter<SavedActivitiesAdapter.ViewHolder> {
     public List<DtoActivity> activities;
     public Activity activityContext;
+    private Executor executor = Executors.newSingleThreadExecutor();
 
     public SavedActivitiesAdapter(Activity activityContext, List<DtoActivity> activities) {
         this.activityContext = activityContext;
@@ -31,6 +34,7 @@ public class SavedActivitiesAdapter extends RecyclerView.Adapter<SavedActivities
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        final int currentPosition = position;
         final DtoActivity activity = activities.get(position);
         StringBuilder value = new StringBuilder().append("Activity Title : ").append(activity.getActivity() + "\n\n").append("Activity Type : ").append(activity.getType() + "\n\n").append("Participants Required : ").append(activity.getParticipants() + "\n\n").append("Price : ").append(activity.getPrice() + "\n\n").append("Activity Link : ").append(activity.getLink() + "\n\n").append("Activity Accessibility : ").append(activity.getAccessibility() + "\n\n");
         holder.tvSavedActivity.setText(value);
@@ -51,8 +55,9 @@ public class SavedActivitiesAdapter extends RecyclerView.Adapter<SavedActivities
         holder.ivDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                activities.remove(position);
-                notifyItemRemoved(position);
+                executor.execute(() -> DatabaseEngine.getInstance().getDBInstance().activityDao().deleteActivity(activity));
+                activities.remove(currentPosition);
+                notifyItemRemoved(currentPosition);
             }
         });
 
